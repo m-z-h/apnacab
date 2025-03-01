29c451e6f791d56f123750d3b8c9f89cdc349100
Md Zahid Hussain <174180646+mdzahidh009@users.noreply.github.com>
2025-03-02 02:05:20 +0530

    modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
-import { Geist, Geist_Mono, Outfit, Inter } from "next/font/google";
+import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "../components/NavBar";
 import React from "react";
 
-const outfit = Outfit({subsets:['latin']})
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({subsets:['latin']});
 
 export default function RootLayout({
   children,
