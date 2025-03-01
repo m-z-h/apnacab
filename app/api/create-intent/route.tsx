89463eacc908ef671109dc055564ee35ac0b8c15
Md Zahid Hussain <174180646+mdzahidh009@users.noreply.github.com>
2025-03-02 02:01:44 +0530

    add Stripe dependency and clean up create-intent route

--- app/api/create-intent/route.tsx
+++ app/api/create-intent/route.tsx
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-
 const stripe =new Stripe(process.env.STRIPE_SECRET_KEY!,{
     typescript:true,
     apiVersion:"2025-02-24.acacia"
